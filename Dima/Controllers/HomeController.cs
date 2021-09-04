eb78d89d96a8898b6f004a00dbc7bc06951d2af1
student <student@DESKTOP-2PJBJBB>
2021-09-04 15:21:17 +0300

    Add project files.

---
+++ Dima/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Dima.Models;
+using Dima.Models.Home;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dima.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            IndexVM vM = new IndexVM() 
+            {
+                MyFullName = "Dima"
+            };
+            return View(vM);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Dima/Models/Home/IndexVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dima.Models.Home
+{
+    public class IndexVM
+    {
+        public string MyFullName { get; set; }
+       
+    }
+}
